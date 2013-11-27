54056b7403d2d1e2e8cce6b1b1435b99f6036fcf
valentin <valentin@kiev>
2013-11-27 20:44:26 +0200

    test

---
+++ newPHPClass.php
@@ -0,0 +1,16 @@
+<?php
+
+/*  ,sdfjg,dg   slfkg s flsfg sf sdfgkjsfd sdf ghfdg,jh sdf  skjhg lfg b,jssfdvf.kbhsf
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author valentin
+ */
+class newPHPClass {
+    //put your code here
+}
